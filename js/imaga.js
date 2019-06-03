01307b1c6f57f2341becf70a77a5f70533fb2cd8
Ilya <36233549+cyberpunk3351@users.noreply.github.com>
2019-06-03 20:49:02 +0700

    add in html random img code

--- js/imaga.js
+++ js/imaga.js
@@ -1,6 +1,6 @@
 function getImage() {
     const random = Math.floor(Math.random() * 4);   
-    return '<img src="img/'+random+'.png" border="0" alt="Sibcomp Service"/>';
+    return '<img src="img/'+random+'.png" border="0" alt="Service banner"/>';
 }
 document.getElementById("imaaaga").innerHTML = getImage();
 document.getElementById("imaaaga2").innerHTML = getImage();
