d58cc174c2629479890743f9a2defbe3fd04408e
Ilya <36233549+cyberpunk3351@users.noreply.github.com>
2020-02-04 22:57:46 +0700

    re:start

--- js/imaga.js
+++
@@ -1,6 +0,0 @@
-function getImage() {
-    const random = Math.floor(Math.random() * 4);   
-    return '<img src="img/'+random+'.png" border="0" alt="Service banner"/>';
-}
-document.getElementById("imaaaga").innerHTML = getImage();
-document.getElementById("imaaaga2").innerHTML = getImage();
--- js/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  $('#animeLogo').hide();
-  $('#animeLogo').each(function(i) {
-          $(this).fadeIn(3000);
-  });
-  });
